d3de57f3fe89d43f73fcb164a710996190204a96
eclipse2101 <rdericieux25@aoiths.org>
2023-11-30 23:23:07 -0500

    gravity now works
    GRAVITY NOW WORKS IN MY GAME AT LAST. LETS FUCKING GOOOOOOOOOOOOOOOOOOOOOOOOO

--- Assets/Scripts/RevampedCharacterController.cs
+++ Assets/Scripts/RevampedCharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))] // this makes sure your character has a character controller before starting the game
 
@@ -8,33 +9,74 @@ public class RevampedCharacterController : MonoBehaviour
 {
     public CharacterController player; // think of this as the motor that controls our player 
     public float speed;
-    private float direction; 
-    public float hInput; 
-    public float vInput;
+    private Vector3 direction; 
+    private Vector2 Input; 
+    public float turnSmoothVelocity;
+    public float turnSmootherTime = 0.1f; 
+    float characterGravity = -9.81f;
+    float gravityScaler = 3.0f;
+    public float velocity;
 
     
-    // Start is called before the first frame update
+    
     void Awake()
     {
         player = GetComponent<CharacterController>();
     }
+    
+    
+    
 
     // Update is called once per frame
     void Update()
     {
-        //// CHARACTER MOVEMENT////
+      
+      GravityFunction();
+      RotationFunction();
+      MovementFunction();
         
-        hInput = Input.GetAxisRaw("Horizontal"); 
-        vInput = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(hInput, 0f, vInput).normalized;
-
-        player.Move(direction * speed * Time.deltaTime);
-
-
-        //// CHARACTER ROTATION////
+    } 
+    
+    //// Applying Gravity ///////
+    private void GravityFunction()
+    {
+         if (player.isGrounded && velocity < 0.0f)// it checks if the player is grounded or not
+        {
+            velocity = -1.0f;
+          
+        }
+        else
+        {
+            velocity += characterGravity * gravityScaler * Time.deltaTime;
+        }
+         
+         direction.y = velocity;
+    }
+    
+    //// CHARACTER ROTATION////
+    private void RotationFunction()
+    {
+         if(Input.sqrMagnitude == 0) return; // this will prevent your character from looking straight forward
         
         var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmootherTime);// this helps smooth the rotation turning for our player turning 
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    } 
+
+    //// CHARACTER MOVEMENT////
+    private void MovementFunction()
+    {
+        /// the line of code that makes the character actual useable//
+
+         player.Move(direction * speed * Time.deltaTime);
     }
+    
+    
+    public void Movement(InputAction.CallbackContext context)
+    {
+       Input = context.ReadValue<Vector2>();
+       direction = new Vector3(Input.x, 0f, Input.y).normalized;
+    }
+
 }
+
