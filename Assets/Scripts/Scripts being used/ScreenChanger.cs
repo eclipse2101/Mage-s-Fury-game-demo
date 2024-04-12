d0e50c36a38667a3706d8a4ac3c4bb7a38d76839
explosiveseccc <rdericieux25@aoiths.org>
2024-04-12 13:21:13 -0400

    plz help me

--- Assets/Scripts/Scripts being used/ScreenChanger.cs
+++ Assets/Scripts/Scripts being used/ScreenChanger.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ScreenChanger : MonoBehaviour
 {
     public GameObject ScreenOne;
     public GameObject ScreenTwo; 
+    private Button ScreenChanger1;
  
 
     
     // Start is called before the first frame update
-    void OnClick()
+    void Awake()
     {
-        ScreenOne.gameObject.SetActive(false);
-        ScreenOne.gameObject.SetActive(true);
+       ScreenChanger1 = GetComponent<Button>();
+       ScreenChanger1.onClick.AddListener(ScreenChange);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ScreenChange()
+    {
+        ScreenOne.gameObject.SetActive(false);
+        ScreenOne.gameObject.SetActive(true);
     }
 }
